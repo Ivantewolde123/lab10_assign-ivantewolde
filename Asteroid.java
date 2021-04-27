f143e73915008098a71f96267298f60efc502ded
Ivantewolde123 <ivantewolde@gmail.com>
2021-04-26 20:28:24 -0400

    i added code to destroy asteroids

--- Asteroid.java
+++ Asteroid.java
@@ -1,11 +1,11 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 
 public class Asteroid extends SimulationActor
 {
 
     private boolean destroyed;
- 
+    private int health = 3; 
     
     public Asteroid()
     {
@@ -25,9 +25,48 @@ public class Asteroid extends SimulationActor
 
         if (destroyed == false)
         {
+            SimulationWorld world = (SimulationWorld) getWorld();
+            List<Bullet> bullets = getWorld().getObjects(Bullet.class);
+            
+            for (int i=0; i < bullets.size(); i++)
+            {
+                Bullet bullet = bullets.get(i);
+                
+                Vector2D bulletToAstroid = new Vector2D(bullet.getX() - getX(), bullet.getY() - getY());
+                double distance = bulletToAstroid.magnitude();
+                
+                double bulletRadius = bullet.getImage().getHeight() / 2;
+                double astroidRadius = getImage().getHeight() / 2;
+                
+                if (distance < bulletRadius + astroidRadius)
+                {
+                    // remove the bullet
+                    world.removeObject(bullet);
+                    health = health - 1;
+                    
+                    if (health == 0)
+                    {
+                        getSimulationWorld().addObject(new Explosion(), getX(), getY());
+                        world.removeObject(this);
+                        
+                        destroyed = true;
+                        
+                        return;
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+}
+    
+
+                
+            
             
 
-        }
+        
 
-    }    
-}
+       
+
